e6418ca735a3b82c662331ac29bfbd0d44f91377
veer aryan <43904888+jeezburger@users.noreply.github.com>
2023-12-15 10:18:33 +0530

    Update anagram.js

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,6 +5,19 @@
 */
 
 function isAnagram(str1, str2) {
+  let len1 = str1.length;
+  let len2 = str2.length;
+  if (len1 != len2) {
+    return false;
+  }
+  let newStr1 = str1.toUpperCase().split("").sort().join("");
+  let newStr2 = str2.toUpperCase().split("").sort().join("");
+  if (newStr1 == newStr2) {
+    return true;
+  }
+  else {
+    return false;
+  }
 
 }
 
